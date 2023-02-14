35b8b3a1ee31f87611000b790c560943ff99d255
rscastilho <rcastilho@gmail.com>
2023-02-14 19:14:49 -0300

    configdotEnv

--- src/data/dbConnect.ts
+++ src/data/dbConnect.ts
@@ -14,18 +14,17 @@ import mysql from 'mysql2';
 //   return con;
 // }
 
-
 export const connection = () => {
   const con = mysql.createPool({
-    host: 'us-east.connect.psdb.cloud',
-    user: 'tj2xbswqtczta25wf4gi',
-    password: 'pscale_pw_C9ECO9rDmxsoAPmo6W7OwFyEcbRMEIiFb41nx82LKVW',
-    database: 'mydbonline',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     waitForConnections: true,
- connectionLimit: 3,
- ssl:{
-  "rejectUnauthorized":true
- }
+    connectionLimit: 3,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   });
   return con;
 };
