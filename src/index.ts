c760a95154ea2c78f346de63206755c3e609ce44
rscastilho <rcastilho@gmail.com>
2023-06-03 19:55:06 -0300

    gittest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import app from './application/server/server';
 const port = process.env.PORT || 3333;
-
-
-
+//tetando git
 app.app.listen(port, () => {
   console.log(`app running at http://localhost:${port}`);
 });
